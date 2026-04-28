f529483fd5c9a2886d20a194742f92143545946f
Andrew Hemming <drewhemm@users.noreply.github.com>
2026-04-28 06:56:52 +0100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -14,37 +14,6 @@ resource "aws_iam_role" "admin" {
   name = "aerith-admin"
 }
 
-resource "aws_iam_role_policy" "admin" {
-  name = "aerith-admin"
-  role = aws_iam_role.admin.id
-
-  policy = jsonencode({
-    Statement = [
-      {
-        Action = [
-          "eks:AccessKubernetesApi",
-          "eks:DescribeAddon",
-          "eks:DescribeAddonVersions",
-          "eks:DescribeCluster",
-          "eks:DescribeFargateProfile",
-          "eks:DescribeIdentityProviderConfig",
-          "eks:DescribeNodegroup",
-          "eks:DescribeUpdate",
-          "eks:ListAddons",
-          "eks:ListClusters",
-          "eks:ListFargateProfiles",
-          "eks:ListIdentityProviderConfigs",
-          "eks:ListNodegroups",
-          "eks:ListUpdates",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-    Version = "2012-10-17"
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "admin" {
   role       = aws_iam_role.admin.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
@@ -64,37 +33,6 @@ resource "aws_iam_role" "read_only" {
     Version = "2012-10-17"
   })
   name = aerith-read-only"
-}
-
-resource "aws_iam_role_policy" "read_only" {
-  name = "aerith-read-only"
-  role = aws_iam_role.read_only.id
-
-  policy = jsonencode({
-    Statement = [
-      {
-        Action = [
-          "eks:AccessKubernetesApi",
-          "eks:DescribeAddon",
-          "eks:DescribeAddonVersions",
-          "eks:DescribeCluster",
-          "eks:DescribeFargateProfile",
-          "eks:DescribeIdentityProviderConfig",
-          "eks:DescribeNodegroup",
-          "eks:DescribeUpdate",
-          "eks:ListAddons",
-          "eks:ListClusters",
-          "eks:ListFargateProfiles",
-          "eks:ListIdentityProviderConfigs",
-          "eks:ListNodegroups",
-          "eks:ListUpdates",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-    Version = "2012-10-17"
-  })
 }
 
 resource "aws_iam_role_policy_attachment" "read_only" {
